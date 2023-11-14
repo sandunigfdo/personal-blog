7c5f4b45783ab605e06fc4bb8dca7b139c816404
Sanduni Fernando <sandunigfdo@gmail.com>
2023-11-15 10:23:52 +1300

    Update index and store functions.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
@@ -14,7 +15,9 @@ class PostController extends Controller
      */
     public function index():View
     {
-        return view('posts.index');
+        return view('posts.index', [
+            'posts' => Post::with('user')->latest()->get(),
+        ]);
     }
 
     /**
@@ -28,9 +31,18 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $validated = $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+            'excerpt' => 'required',
+            // 'thumbnail' => $post->exists ? ['image'] : ['required', 'image'],
+        ]);
+
+        $request->user()->posts()->create($validated);
+
+        return redirect(route('posts.index'));
     }
 
     /**
