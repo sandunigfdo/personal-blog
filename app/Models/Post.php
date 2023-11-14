6de84ac1e9fe4a013929d8b43fdd96172ea6ecbf
Sanduni Fernando <sandunigfdo@gmail.com>
2023-11-15 10:25:17 +1300

    Add m-assignment and define the relationship .

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,9 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title', 
+        'excerpt', 
+        'body',
+    ];
+
+    public function user():BelongsTo {
+        return $this->belongsTo(User::class);
+    }
 }
