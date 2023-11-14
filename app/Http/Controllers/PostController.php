544ecaa55a18bd246401fcb032f72a2dc2980f25
Sanduni Fernando <sandunigfdo@gmail.com>
2023-11-14 23:30:35 +1300

    Add create function to create a new post.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,9 +20,9 @@ class PostController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
-        //
+        return view('posts.create');
     }
 
     /**
