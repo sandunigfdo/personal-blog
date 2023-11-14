84957a75c6175bc43e893dfedec164511a563b51
Sanduni Fernando <sandunigfdo@gmail.com>
2023-11-14 23:32:53 +1300

    Reformat and update padding to outer div.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,10 +1,10 @@
 <x-app-layout>
-    <div class="bg-white py-24 sm:py-32">
+    <div class="bg-white py-12 sm:py-12">
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
 
             <div class="mx-auto max-w-2xl lg:mx-0">
-                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
-                <p class="mt-2 text-lg leading-8 text-gray-600">Learn how to grow your business with our expert advice.</p>
+                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Stay curious.</h2>
+                <p class="mt-2 text-lg leading-8 text-gray-600">Discover stories.</p>
             </div>
 
             <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
@@ -13,14 +13,14 @@
                     
                     <div class="flex items-center gap-x-4 text-xs">
                         <time datetime="2020-03-16" class="text-gray-500">Mar 16, 2020</time>
-                        <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Marketing</a>
+                        <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Category</a>
                     </div>
 
                     <div class="group relative">
                         <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
                             <a href="#">
                             <span class="absolute inset-0"></span>
-                            Boost your conversion rate
+                            This is the Post title.
                             </a>
                         </h3>
                         <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
@@ -105,7 +105,7 @@
                         </div>
                     </div>
 
-                </article>
+                </article>                
 
             </div>
         </div>
