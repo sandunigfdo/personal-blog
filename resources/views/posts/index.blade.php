583df533373de61e8ca00b216f01b0bb56276977
Sanduni Fernando <sandunigfdo@gmail.com>
2023-11-17 00:21:00 +1300

    Reformat to match with other templates.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,13 +1,18 @@
 <x-app-layout>
-    <div class="bg-white py-12 sm:py-12">
-        <div class="mx-auto max-w-7xl px-6 lg:px-8">
-
-            <div class="mx-auto max-w-2xl lg:mx-0">
-                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Stay curious.</h2>
-                <p class="mt-2 text-lg leading-8 text-gray-600">Discover stories.</p>
+    <div class="pb-12 sm:pb-12">
+        <div class="mx-auto max-w-6xl sm:px-6 px-6 lg:px-8 space-y-6">
+            
+            <div class="max-w-6xl mx-auto lg:px-8">
+                <div class="border-b border-gray-900/10 pb-8">
+                    <div class="mx-auto max-w-2xl lg:mx-0">
+                        <h2 class="mt-8 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Stay curious.</h2>
+                        <p class="mt-2 text-lg leading-8 text-gray-600">Discover stories.</p>
+                    </div>
+                </div>
             </div>
+                                  
 
-            <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            <div class="mx-auto mt-8 grid max-w-3xl grid-cols-1 gap-x-12 gap-y-16 sm:mt-16 sm:p-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
             
                 @foreach ($posts as $post)
                     <article class="flex max-w-xl flex-col items-start justify-between">
@@ -18,25 +23,25 @@
                         </div>
 
                         <div class="group relative">
-                            <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                                <a href="#">
+                            <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 hover:text-gray-500">
+                                <a href="/posts/{{ $post->id }}">
                                 <span class="absolute inset-0"></span>
                                 {{ $post->title }}
                                 </a>
                             </h3>
-                            <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">                                
+                            <p class="mt-5 line-clamp-3 text-sm leading-6 text-black text-justify">                                
                             {{ $post->excerpt }}
                             </p>
-                        </div>
+                        </div>                        
 
                         <div class="relative mt-8 flex items-center gap-x-4">            
                             <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="h-10 w-10 rounded-full bg-gray-50">
                             <div class="text-sm leading-6">
                                 <p class="font-semibold text-gray-900">
-                                <a href="#">
-                                    <span class="absolute inset-0"></span>
-                                    {{ $post->user->name }}
-                                </a>
+                                    <a href="#">
+                                        <span class="absolute inset-0"></span>
+                                        {{ $post->user->name }}
+                                    </a>
                                 </p>                                
                             </div>
                         </div>
@@ -54,13 +59,15 @@
                     </div>
 
                     <div class="group relative">
-                        <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                        <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 hover:text-gray-500">
                             <a href="#">
                             <span class="absolute inset-0"></span>
                             Boost your conversion rate
                             </a>
                         </h3>
-                        <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
+                        <p class="mt-5 line-clamp-3 text-sm leading-6 text-black text-justify">
+                            Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.
+                        </p>
                     </div>
 
                     <div class="relative mt-8 flex items-center gap-x-4">            
@@ -86,13 +93,15 @@
                     </div>
 
                     <div class="group relative">
-                        <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                        <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 hover:text-gray-500">
                             <a href="#">
                             <span class="absolute inset-0"></span>
                             Boost your conversion rate
                             </a>
                         </h3>
-                        <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
+                        <p class="mt-5 line-clamp-3 text-sm leading-6 text-black text-justify">
+                            Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.
+                        </p>
                     </div>
 
                     <div class="relative mt-8 flex items-center gap-x-4">            
