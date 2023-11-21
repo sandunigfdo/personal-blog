9c2eb116f7dfa33bc636c0922065eef291e514a2
Sanduni Fernando <sandunigfdo@gmail.com>
2023-11-21 23:18:43 +1300

    Reformat the code.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -51,7 +51,7 @@
                                             <textarea 
                                                 id="excerpt" 
                                                 name="excerpt" 
-                                                rows="3" 
+                                                rows="4" 
                                                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                                             </textarea>
                                         </div>                            
@@ -94,7 +94,7 @@
                     </div>
 
                     <div class="mt-6 flex items-center justify-end gap-x-6">
-                        <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+                    <a href="{{ route('posts.index') }}" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
                         <button type="submit" class="rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">Save</button>
                     </div>
 
