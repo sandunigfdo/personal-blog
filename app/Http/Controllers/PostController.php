3c27d29afcecbcbf5a96ce687e6e444b6a75926b
Sanduni Fernando <sandunigfdo@gmail.com>
2023-11-21 23:17:49 +1300

    Add the destroy function.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -88,8 +88,12 @@ class PostController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Post $post)
+    public function destroy(Post $post): RedirectResponse
     {
-        //
+        $this->authorize('delete', $post);
+
+        $post->delete();
+        
+        return redirect(route('posts.index'));
     }
 }
