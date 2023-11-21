5d7c43862119fc33169dfa273b0600690e81c316
Sanduni Fernando <sandunigfdo@gmail.com>
2023-11-21 23:19:03 +1300

    Add a Cancel button.

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -99,7 +99,7 @@
 
                     <x-input-error :messages="$errors->get('message')" class="mt-2" />
                     <div class="mt-6 flex items-center justify-end gap-x-6">
-                        <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+                        <a href="{{ route('posts.show', $post) }}" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
                         <button type="submit" class="rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">Update</button>
                     </div>        
             </form>
