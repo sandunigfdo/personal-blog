45f041244d187c16627811d68f68bd72dc7aa07c
Sanduni Fernando <sandunigfdo@gmail.com>
2023-11-21 23:40:21 +1300

    Link the thumbnail to the profile.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -43,10 +43,18 @@
                             <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="h-10 w-10 rounded-full bg-gray-50">
                             <div class="text-sm leading-6">
                                 <p class="font-semibold text-gray-900">
-                                    <a href="#">
+                                @if ($post->user->is(auth()->user()))   
+                                    <a href="/profile">                                        
                                         <span class="absolute inset-0"></span>
                                         {{ $post->user->name }}
                                     </a>
+                                @else
+                                    <a href="{{ route('posts.index') }}">                                        
+                                        <span class="absolute inset-0"></span>
+                                        {{ $post->user->name }}                                    
+                                    </a>
+                                @endif
+                                    
                                 </p>                                
                             </div>
                         </div>
