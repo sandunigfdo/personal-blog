089ae45a84f8c1f0c1b2ba56409568cff5bec4a2
Sanduni Fernando <sandunigfdo@gmail.com>
2023-11-24 11:02:12 +1300

    Update styling of the page.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -12,7 +12,7 @@
             </div>
 
             <div class="max-w-3xl mx-auto sm:px-6 px-6 lg:px-8 space-y-6"> 
-                <div class="border-b border-gray-900/10 pb-8">                    
+                <div class="pb-8">                    
                     <div class="p-4 sm:p-8 bg-white sm:rounded-lg">
 
                         <article class="max-w-xl items-start">              
@@ -60,7 +60,7 @@
                                 </p>
                             </div>                        
 
-                            <div class="relative mt-8 flex items-center gap-x-4">            
+                            <div class="relative mt-8 flex items-center gap-x-4 border-b border-gray-900/10 pb-8 max-w-3xl mx-auto sm:px-6 px-6 lg:px-8">            
                                 <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="h-10 w-10 rounded-full bg-gray-50">
                                 <div class="text-sm leading-6">
                                     <p class="font-semibold text-gray-900">
@@ -77,6 +77,44 @@
                                     @endif
                                     </p>                                
                                 </div>
+                            </div>
+                            
+                            <div class="max-w-3xl mx-auto sm:p-6 p-6 lg:p-8  ">
+                                <form method="POST" action="/posts/{{ $post->id }}/comments">
+                                    @csrf
+                                    <textarea
+                                        name="body"
+                                        placeholder="{{ __('What are your thoughts?') }}"
+                                        class="block w-full border-gray-300 focus:border-gray-500 focus:ring focus:ring-gray-300 focus:ring-opacity-50 rounded-md text-sm"
+                                    >{{ old('body') }}</textarea>
+                                    <x-input-error :messages="$errors->get('body')" class="mt-2" />
+                                    <div class="mt-8 flex justify-end">
+                                        <button
+                                            type="submit" 
+                                            class="rounded-md bg-blue-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                                        >{{ __('Respond') }}
+                                    </button>
+                                    </div>
+                                </form>
+                            </div>  
+                            
+                            <div class="max-w-3xl mx-auto sm:p-6 p-6 lg:p-8">
+                                @foreach ($post->comments as $comment)
+                                    <div class="p-4 mt-8 flex space-x-2 rounded-md border border-gray-300 ">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-600 -scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+                                        </svg>
+                                        <div class="flex-1">
+                                            <div class="flex justify-between items-center">
+                                                <div>
+                                                    <span class="text-sm text-gray-800">{{ $comment->user->name }}</span>
+                                                    <time class="ml-2 text-xs text-gray-600">{{ $comment->created_at->diffForHumans() }}</time>
+                                                </div>
+                                            </div>
+                                            <p class="mt-4 text-sm text-gray-900">{{ $comment->body }}</p>
+                                        </div>
+                                    </div>
+                                @endforeach
                             </div>
 
                         </article>
