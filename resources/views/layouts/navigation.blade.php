4073492bd8558df19ec0c1cc2c0630d94ea42682
Sanduni Fernando <sandunigfdo@gmail.com>
2024-02-09 00:10:02 +1300

    Remove additional spacing.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,9 +12,6 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    
-                    
-
                     <x-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
                         {{ __('Posts') }}
                     </x-nav-link>
