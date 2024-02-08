1e7204a65e1a2d1053c142a26535b7cfdd8af89c
Sanduni Fernando <sandunigfdo@gmail.com>
2024-02-09 00:09:43 +1300

    Create a Controller class to category.

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Post;
+use App\Models\Category;
+use Illuminate\View\View;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CategoryController extends Controller
+{
+    
+    public function index(Category $category): View
+    {
+        return view('posts.index',[
+            'posts' => $category->posts,
+            'currentCategory' => $category,
+            'categories' => Category::all()
+        ]);
+    }
+}
