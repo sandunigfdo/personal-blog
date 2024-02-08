5214537d44218d90c67f268bf2aeb733af4c6bfe
Sanduni Fernando <sandunigfdo@gmail.com>
2024-02-09 00:10:40 +1300

    Add a dropdown menu to filter posts by category.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,9 +5,42 @@
             
             <div class="max-w-6xl mx-auto lg:px-8">
                 <div class="border-b border-gray-900/10 pb-8">
-                    <div class="mx-auto max-w-2xl lg:mx-0">
-                        <h2 class="mt-8 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Stay curious.</h2>
-                        <p class="mt-2 text-lg leading-8 text-gray-600">Discover stories.</p>
+                    <div class="mt-8 mx-auto max-w-2xl lg:mx-0 mb-8">
+                        <!-- <h2 class="mt-8 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Stay curious.</h2>
+                        <p class="mt-2 text-lg leading-8 text-gray-600">Discover stories.</p> -->
+                        
+                        <!-- Category Dropdown -->
+                        <div x-data="{ show: false }" @click.away="show = false">
+                            <button 
+                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                @click="show = ! show"  >                           
+                                
+                                    <div>{{ isset($currentCategory) ?  ucwords($currentCategory->name) : 'Categories' }}</div>
+                                    <div class="ms-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                            </button> 
+
+                            <div x-show="show" class="py-2 absolute bg-gray-100 w-32 mt-2 z-50" style="display: none">
+                                    <a  href="#" 
+                                        class="block text-left px-3 text-sm leading-6 hover:bg-blue-400 focus:bg-blue-400 hover:text-white focus:text-white">
+                                        All
+                                    </a>
+                                @foreach ($categories as $category)
+                                    <a  href="/categories/{{ $category->id }}" 
+                                        class="block text-left px-3 text-sm leading-6 hover:bg-blue-400 focus:bg-blue-400 hover:text-white focus:text-white
+                                        {{ isset($currentCategory) && $currentCategory->id == $category->id ? 'bg-blue-400 text-white' : '' }} ">
+                                        
+                                        {{ ucwords($category->name)}}
+                                    </a>
+                                @endforeach
+                                
+                            </div>
+                        </div>
+                        
+
                     </div>
                 </div>
             </div>
