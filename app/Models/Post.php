705fd8181b3834fbb61f76db9cad96dea0d41578
Sanduni Fernando <sandunigfdo@gmail.com>
2024-02-10 00:47:13 +1300

    Define the local query scope filter.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Builder;
 
 class Post extends Model
 {
@@ -30,5 +31,14 @@ class Post extends Model
 
     public function category():BelongsTo {
         return $this->belongsTo(Category::class);
+    }
+
+    public function scopeFilter(Builder $query, array $filters): void {
+
+        if($filters['search'] ?? false){
+            $query
+                ->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('body', 'like', '%' . request('search') . '%');
+        }
     }
 }
